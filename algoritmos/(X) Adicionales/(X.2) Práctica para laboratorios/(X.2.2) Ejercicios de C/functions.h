6188e364d8c667dac47b40308b6551c6c56401ee
Lucas Ivan Benchat Parra <libenchat@hotmail.com>
2026-07-19 01:26:51 -0300

    [algoritmos] Modificación de functions.h
    Se reacomodaron algunas importaciones de librerías y definiciones condicionales de variables para que estén en el lugar correcto.
    
    También se modificó la lógica de obtención de números al azar en get_random_number() para que la función no devuelva números negativos si no está permitido.
    
    Esto también requirió modificar cargar_arreglo_enteros() para respetar la nueva definición de get_random_number().

--- algoritmos/(X) Adicionales/(X.2) Práctica para laboratorios/(X.2.2) Ejercicios de C/functions.h
+++ algoritmos/(X) Adicionales/(X.2) Práctica para laboratorios/(X.2.2) Ejercicios de C/functions.h
@@ -8,26 +8,66 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+// clang-format off
+#ifdef _WIN32
+    // Macro específico de Windows que debe ser definido
+    // antes de importar la librería "stdlib".
+    #define _CRT_RAND_S
+#endif
+
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int get_random_number() {
+#ifdef __linux__
+    #include <sys/random.h>
+#elif !defined(_WIN32)
+    #include <time.h>
+#endif
+// clang-format on
+
+int get_random_number(bool get_negative_num) {
     int num;
 
     // clang-format off
     #ifdef _WIN32
-        #define _CRT_RAND_S
         rand_s(&num);
     #elif __linux__
-        #include <sys/random.h>
         getrandom(&num, sizeof(num), 0);
     #else
-        #include <time.h>
         srand(time(NULL));
         num = rand();
     #endif
     // clang-format on
+
+    if (!get_negative_num && num < 0) {
+        // En caso de que se obtenga un número negativo pero
+        // no se pueda trabajar con números negativos se aplica
+        // una máscara binaria para obtener el valor absoluto
+        // del número.
+        //
+        // La conversión se realiza de esta manera y no con
+        // la función abs() de "stdlib.h" porque abs() no es
+        // capaz de devolver correctamente el valor absoluto
+        // de INT_MIN (-2.147.483.648), que es uno de los
+        // valores que se podrían obtener tratando de llenar
+        // 32 bits de espacio con valores al azar.
+        //
+        // Dicha operación resulta exactamente en el mismo
+        // valor o en conducta indefinida, dependiendo del
+        // compilador utilizado, porque los 32 bits del
+        // tipo "int" no alcanzan para representar una
+        // versión positiva del valor INT_MIN.
+        //
+        // Esta máscara es un valor en hexadecimal donde
+        // todos los bits, salvo el más significativo,
+        // tienen valor 1, de manera que se conserva el
+        // valor de todos los bits correspondientes al
+        // valor de la variable "num", salvo el del bit
+        // del signo.
+        num = num & 0x7FFFFFFF;
+    }
 
     return num;
 }
@@ -35,6 +75,7 @@ int get_random_number() {
 void cargar_arreglo_enteros(int* arr, size_t arr_size, int low_lim,
                             int upp_lim) {
     assert(low_lim != upp_lim);
+    bool allow_negative_values = false;
 
     for (size_t i = 0; i < arr_size; i++) {
         // Explicación de la fórmula utilizada:
@@ -44,7 +85,13 @@ void cargar_arreglo_enteros(int* arr, size_t arr_size, int low_lim,
         //    un número entre 0 y la cantidad de posibles números a obtener.
         // 3. Al resultado del módulo se le suma el valor del límite
         //    inferior para que por lo menos sea igual a dicho número.
-        arr[i] = (get_random_number() % (upp_lim - low_lim + 1)) + low_lim;
+        if (low_lim < 0) {
+            allow_negative_values = true;
+        }
+
+        arr[i] = (get_random_number(allow_negative_values) %
+                  (upp_lim - low_lim + 1)) +
+                 low_lim;
     }
 }
 
